9104f8994786ad1ba4bf942558874a6858568231
dariiapikul <dariyapikul@gmail.com>
2020-10-09 12:06:55 +0300

    MapsId used

--- src/main/java/com/dev/cinema/model/ShoppingCart.java
+++ src/main/java/com/dev/cinema/model/ShoppingCart.java
@@ -2,9 +2,9 @@ package com.dev.cinema.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -13,10 +13,11 @@ import javax.persistence.Table;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
     private List<Ticket> tickets;
+    @MapsId
+    @JoinColumn(name = "id")
     @OneToOne
     private User user;
 
