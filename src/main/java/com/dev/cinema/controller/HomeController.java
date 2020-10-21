f827cd10ee9c33753bde668c61ea56d5cc4f6c29
dariiapikul <dariyapikul@gmail.com>
2020-10-21 20:27:13 +0300

    Checkstyle

--- src/main/java/com/dev/cinema/controller/HomeController.java
+++ src/main/java/com/dev/cinema/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
     @GetMapping("/home")
     public String printHome() {
         return "Home Page";
